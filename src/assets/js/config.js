d087a470d43f399936c7d9561f7e3444074c9276
Aleksandar Djordjevic <adjordjevic@firstbeatmedia.com>
2018-11-13 00:25:24 +0100

    Updating project code style

--- src/assets/js/config.js
+++ src/assets/js/config.js
@@ -1,54 +1,55 @@
-(function() {
-  var options;
+(function () {
+    var options;
 
-  options = {
-    waitSeconds: 60,
-    baseUrl: "assets/js",
-    paths: {
-      "jquery": "../libs/jquery/jquery",
-      "jquery-ui": "../libs/jquery-ui/jquery-ui.min",
-      "jquery-cookie": "../libs/jquery-cookie/jquery.cookie",
-      "angular": "../libs/angular/angular",
-      "ngRoute": "../libs/angular-route/angular-route",
-      "angular-bootstrap": "../libs/angular-bootstrap/ui-bootstrap-tpls.min",
-      "ngAnimate": "../libs/angular-animate/angular-animate.min",
-      "lodash": "../libs/lodash/dist/lodash.min",
-      "moment": "../libs/moment/min/moment.min",
-      "bootstrap": "bootstrap.min"
-    },
-    shim: {
-      "angular": {
-        exports: 'angular'
-      },
-      "ngRoute": [
-        "angular"
-      ],
-      "angular-bootstrap": {
-        deps: ["angular"],
-        exports: 'angular.bootstrap'
-      },
-      "ngAnimate": {
-        deps: ["angular"],
-        exports: 'ngAnimate'
-      },
-      "bootstrap": {
-        deps: ['jquery']
-      },
-      "main": {
-        deps: ['jquery']
-      }
-    },
-    deps: ['../../app/app']
-  };
+    options = {
+        waitSeconds: 60,
+        baseUrl: "assets/js",
+        paths: {
+            "jquery": "../libs/jquery/jquery",
+            "jquery-ui": "../libs/jquery-ui/jquery-ui.min",
+            "jquery-cookie": "../libs/jquery-cookie/jquery.cookie",
+            "angular": "../libs/angular/angular",
+            "ngRoute": "../libs/angular-route/angular-route",
+            "angular-bootstrap": "../libs/angular-bootstrap/ui-bootstrap-tpls.min",
+            "ngAnimate": "../libs/angular-animate/angular-animate.min",
+            "lodash": "../libs/lodash/dist/lodash.min",
+            "moment": "../libs/moment/min/moment.min",
+            "bootstrap": "bootstrap.min"
+        },
+        shim: {
+            "angular": {
+                exports: 'angular'
+            },
+            "ngRoute": [
+                "angular"
+            ],
+            "angular-bootstrap": {
+                deps: ["angular"],
+                exports: 'angular.bootstrap'
+            },
+            "ngAnimate": {
+                deps: ["angular"],
+                exports: 'ngAnimate'
+            },
+            "bootstrap": {
+                deps: ['jquery']
+            },
+            "main": {
+                deps: ['jquery']
+            }
+        },
+        deps: ['../../app/app']
+    };
 
-  if (typeof define === "function" && (define.amd != null)) {
-  } else if (typeof module !== "undefined" && (module.exports != null)) {
-    module.exports = options;
-  }
+    if (typeof define === "function" && (define.amd != null)) {
+    } else if (typeof module !== "undefined" && (module.exports != null)) {
+        module.exports = options;
+    }
 
-  if (require.config != null) {
-    require.config(options);
-    require(["main"], function() {});
-  }
+    if (require.config != null) {
+        require.config(options);
+        require(["main"], function () {
+        });
+    }
 
 }).call(this);
